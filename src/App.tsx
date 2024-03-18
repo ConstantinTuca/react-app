eb451144e79d525e6da3972167b6458d0e3f4fc3
Constantin Tuca <costi.tuca@gmail.com>
2024-03-18 14:37:08 +0200

    continue learning

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import ListGroup from "./components/ListGroup";
+const items = [
+  'New york',
+  'Constanta',
+  'Bucuresti',
+  'Suceava'
+];
+
+const handleSelectItem = (item: string) => {
+  console.log(item);
+};
 
 function App() {
-  return <div><ListGroup></ListGroup></div>
+  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}></ListGroup></div>
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,25 +1,29 @@
 import { useState } from "react";
 
-function ListGroup() {
-  const items = [
-    'New york',
-    'Constanta',
-    'Bucuresti',
-    'Suceava'
-  ];
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
   // Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       { items.length === 1 && <p>No items found</p> }
       <ul className="list-group">
         {items.map((item, index) => (
           <li
             className={ selectedIndex === index ? "list-group-item active" : "list-group-item"}
             key={item}
-            onClick = { () => setSelectedIndex(index) }>{item}
+            onClick = {() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+              }}
+            >{item}
           </li>
         ))}
       </ul>
